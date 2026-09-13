fb9f9424008af82a3e180b31c0d08920d2a364ca
Annapurnabht <annapurnabht77@gmail.com>
2026-09-14 00:30:48 +0600

    Added arthimetic

---
+++ c operators/Arthimetic .c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main() 
+{
+int a=10,b=3;
+printf("Sum=%d\n",a+b);
+printf("Difference=%d\n",a-b);
+printf("Product=%d\n",a*b);
+printf("Division=%d\n",a/b);
+printf("Remainder=%d",a%b);
+return 0;
+}
