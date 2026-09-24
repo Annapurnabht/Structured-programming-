b03220d086f3ee280b9f39e4b969e91299d7ae26
Annapurnabht <annapurnabht77@gmail.com>
2026-09-25 00:54:00 +0600

    Added

---
+++ C FUNCTIONS/C math function.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    printf("Square root = %.2f\n", sqrt(25));
+    printf("Power = %.2f\n", pow(2, 3));
+    printf("Absolute value = %d\n", abs(-10));
+
+    return 0;
+}
