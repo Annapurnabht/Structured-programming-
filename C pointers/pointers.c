6d6fe03dc854101bd7390f548ff81f9828476cff
Annapurnabht <annapurnabht77@gmail.com>
2026-09-24 19:48:12 +0600

    Added

---
+++ C pointers/pointers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int x = 10;
+    int *p;
+
+    p = &x;
+
+    printf("Value of x = %d\n", x);
+    printf("Address of x = %p\n", (void*)&x);
+    printf("Value using pointer = %d\n", *p);
+
+    return 0;
+}
