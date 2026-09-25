1c958cb3e54cd361eae6dc042c85f8737940df19
Annapurnabht <annapurnabht77@gmail.com>
2026-09-25 22:12:48 +0600

    Added

---
+++ C MORE/C bitwise operators.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int a = 5;
+    int b = 3;
+
+    printf("AND = %d\n", a & b);
+    printf("OR = %d\n", a | b);
+    printf("XOR = %d\n", a ^ b);
+
+    return 0;
+}
