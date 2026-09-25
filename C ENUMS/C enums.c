f37b9bc232620b06d9016626bf177e731178443a
Annapurnabht <annapurnabht77@gmail.com>
2026-09-26 01:10:58 +0600

    Added

--- C ENUMS/C enums.c
+++ C ENUMS/C enums.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
 
+enum Week {
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday,
+    Sunday
+};
+
+int main() {
+    enum Week today;
+
+    today = Wednesday;
+
+    printf("Today is day number: %d\n", today);
+
+    return 0;
+}
