89aa32d4f96e85948d46997155212608c5b68611
Annapurnabht <annapurnabht77@gmail.com>
2026-09-25 22:09:08 +0600

    Added

---
+++ C MORE/C organize code.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void printHello() {
+    printf("Hello\n");
+}
+
+void printNumber() {
+    printf("Number = 10\n");
+}
+
+int main() {
+    printHello();
+    printNumber();
+
+    return 0;
+}
