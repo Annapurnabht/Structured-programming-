266015b180c7e1d13cb8826392eabc13cd7858d3
Annapurnabht <annapurnabht77@gmail.com>
2026-09-25 20:24:01 +0600

    Added

---
+++ C ERRORS/C debugging.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int a = 10;
+    int b = 20;
+
+    printf("Value of a = %d\n", a);
+    printf("Value of b = %d\n", b);
+
+    int sum = a + b;
+
+    printf("Sum = %d\n", sum);
+
+    return 0;
+}
