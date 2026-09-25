21f9abaf7133c55033790940b43cc812182c0c45
Annapurnabht <annapurnabht77@gmail.com>
2026-09-25 21:46:04 +0600

    Added

--- C MORE/C data.c
+++ C MORE/C data.c
@@ -1,1 +1,12 @@
+#include <stdio.h>
+#include <time.h>
 
+int main() {
+    time_t currentTime;
+
+    time(&currentTime);
+
+    printf("Current date and time: %s", ctime(&currentTime));
+
+    return 0;
+}
