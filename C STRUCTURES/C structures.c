b1d572c6f7ee952eb5fa132a974da02a6a6d6bd9
Annapurnabht <annapurnabht77@gmail.com>
2026-09-25 17:30:55 +0600

    Added

--- C STRUCTURES/C structures.c
+++ C STRUCTURES/C structures.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
 
+struct Student {
+    int roll;
+    char name[20];
+    float gpa;
+};
+
+int main() {
+    struct Student s1 = {101, "Anuradha", 3.75};
+
+    printf("Roll: %d\n", s1.roll);
+    printf("Name: %s\n", s1.name);
+    printf("GPA: %.2f\n", s1.gpa);
+
+    return 0;
+}
