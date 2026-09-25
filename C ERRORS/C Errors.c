a743a049ad6b1e6e622a8940168b0d8b6b4c1c6c
Annapurnabht <annapurnabht77@gmail.com>
2026-09-25 20:22:04 +0600

    Added

--- C ERRORS/C Errors.c
+++ C ERRORS/C Errors.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
 
+int main() {
+    int a = 10, b = 0;
+
+    if (b == 0) {
+        printf("Error: Cannot divide by zero.\n");
+    } else {
+        printf("Result = %d", a / b);
+    }
+
+    return 0;
+}
